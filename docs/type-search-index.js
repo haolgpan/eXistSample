27d6db7b5c86224787c8a71a2b71752000a6045c
Hao <haolgpan@gmail.com>
2023-04-23 21:15:22 +0200

    JavaDoc generated updated

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Controller","l":"ExistController"},{"p":"<Unnamed>","l":"Main"},{"p":"View","l":"Menu"}];updateSearchResults();
